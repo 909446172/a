cfd5014efaf9569de5b14942b3653e86a280867b
17608449089 <17608449089@163.com>
2019-10-21 09:55:17 +0800

    10/14 15:33

--- demo/src/main/java/com/example/demo/controller/TestController.java
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2019/10/14 15:23
  * @Version 1.0
  */
+@Slf4j
 @RestController
 public class TestController {
 
 
     @RequestMapping("/{test}")
     public String test(@PathVariable String test) {
+        log.debug(test);
+        log.info(test);
+        log.warn(test);
+        log.error(test);
         return test;
     }
 
